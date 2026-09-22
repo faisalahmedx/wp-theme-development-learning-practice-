f15aaf7c34ca607f8666da620436a976e77b0266
faisalahmedx <faisal881140@gmail.com>
2026-09-23 01:24:39 +0600

    update

--- inc/custompost-t.php
+++ inc/custompost-t.php
@@ -30,3 +30,36 @@ if(!function_exists('practice_custom_post')){
 }
 
 add_action('init','practice_custom_post');
+
+
+
+
+
+// 1. Mission Post Type-er jonno Custom Taxonomy Register Kora
+function create_mission_custom_taxonomy() {
+    $labels = array(
+        'name'              => __('Mission Categories','practice-theme'),
+        'singular_name'     => __('Mission Category','practice-theme'),
+        'search_items'      => __('Search Categories','practice-theme'),
+        'all_items'         => 'All Categories',
+        'parent_item'       => 'Parent Category',
+        'parent_item_colon' => 'Parent Category:',
+        'edit_item'         => 'Edit Category',
+        'update_item'       => 'Update Category',
+        'add_new_item'      => 'Add New Category',
+        'new_item_name'     => 'New Category Name',
+        'menu_name'         => 'Categories',
+    );
+
+    $args = array(
+        'hierarchical'      => true, // true dile Category-r moto kaj korbe (checkbox thakbe)
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true, // Dashboard-e post list column-e category dekhabe
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'mission-category' ), // URL Slug
+    );
+
+    register_taxonomy( 'mission_cat', array( 'our-mission' ), $args );
+}
+add_action( 'init', 'create_mission_custom_taxonomy' );
